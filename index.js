8691471a3391a3791954227149f8e5e634137089
joseragessi <116849621+joseragessi@users.noreply.github.com>
2026-03-29 11:50:11 -0300

    Implement CORS middleware in index.js
    Added CORS headers to allow cross-origin requests.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { procesarMensaje } = require('./conversacion');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use((req,res,next)=>{res.header('Access-Control-Allow-Origin','*');res.header('Access-Control-Allow-Headers','Content-Type');res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS');if(req.method==='OPTIONS')return res.sendStatus(200);next();});
 
 const twilioClient = twilio(
   process.env.TWILIO_ACCOUNT_SID,
